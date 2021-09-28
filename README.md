175b169707f24600a475aaffa40350db71cf0fda
Raul <rgomezbe@cisco.com>
2021-09-27 21:54:24 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# PEXPECT VS NETMIKO
+
+## Description
+
+Test script to demonstrate the differences between python modules
+
+It uses both modules to connect to a test router and obtain a sample command
+
+## Author
+
+Raul Gomez
+
+    raul.agobe@gmail.com
---
+++ pexpectVSnetmiko.py
@@ -0,0 +1,56 @@
+from netmiko import ConnectHandler
+import pexpect
+import sys
+
+
+def pe_show_command(device,command):
+
+    enable_prompt = device["host"] + '#'
+    ssh = pexpect.spawn(f'ssh -l {device["username"]} -p {device["port"]} {device["ip"]}')
+
+    # ssh.logfile_read = sys.stdout.buffer  # This line will show output while connecting
+    
+    ssh.expect('Password:')
+    ssh.sendline(device["password"])
+    ssh.expect_exact(enable_prompt)
+    ssh.sendline('show ip int brief')
+    ssh.expect_exact(enable_prompt)
+    result = (ssh.before).decode("utf-8")
+    ssh.close()
+
+    return result
+
+
+def nm_show_command(device,command):
+
+    ssh = ConnectHandler(**device)
+    result = ssh.send_command(command)
+    ssh.disconnect()
+
+    return result
+
+
+if __name__ == '__main__':
+
+    c8000v  = {
+            'device_type': 'cisco_ios',
+            'ip': '10.88.255.98',
+            'port': 22051,
+            'username': 'admin',
+            'password': 'admin',
+            'host': 'c8000v_Flask',
+        }
+
+    print('\nTesting device:')
+    for k,v in c8000v.items():
+        print(k + ': ' + str(v))
+
+    print('\nUsing PEXPECT')
+    print('=====================\n')
+    output = pe_show_command(c8000v,'show ip int brief')
+    print(output)
+
+    print('\nUsing NETMIKO')
+    print('=====================\n')
+    output = nm_show_command(c8000v,'show ip int brief')
+    print(output)
